da19f65d00f01fc0ae9e061af4818da401b0ca0a
Tetsuya Mori <monry84@gmail.com>
2018-05-03 23:48:20 +0900

    :sparkles: Implement GeneratorExtension
    * CAFU Generator で Entity を作成する際に処理をインジェクションする

---
+++ Assets/Editor/GeneratorExtension/GenericEntity.cs
@@ -0,0 +1,42 @@
+﻿using CAFU.Generator;
+using CAFU.Generator.Enumerates;
+using JetBrains.Annotations;
+using UnityEditor;
+
+namespace CAFU.Generics.GeneratorExtension
+{
+    [PublicAPI]
+    internal enum EntityType
+    {
+        None = 0,
+        GenericEntity,
+        ScriptableObjectGenericEntity,
+    }
+
+    [InitializeOnLoad]
+    public class GenericEntity : IClassStructureExtension
+    {
+        private EntityType EntityType { get; set; }
+
+        static GenericEntity()
+        {
+            var instance = new GenericEntity();
+            GeneratorWindow.RegisterAdditionalOptionRenderDelegate(LayerType.Entity, instance);
+            GeneratorWindow.RegisterAdditionalStructureExtensionDelegate(LayerType.Entity, instance);
+        }
+
+        public void OnGUI()
+        {
+            EntityType = (EntityType)EditorGUILayout.EnumPopup("Entity Type", EntityType);
+        }
+
+        public void Process(Parameter parameter)
+        {
+            if (EntityType > EntityType.None)
+            {
+                parameter.UsingList.Add("CAFU.Generics.Data.Entity");
+                parameter.BaseClassName = EntityType.ToString();
+            }
+        }
+    }
+}
