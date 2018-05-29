243bff3ff781934eb752a434f41c156d842c8b96
Tetsuya Mori <monry84@gmail.com>
2018-05-29 20:37:27 +0900

    :sparkles: Implements GenericEntityContainerDataStore
    * Stores instances of IGenericEntity
    * Support reference counter
    * Support Dependency Injections

---
+++ Assets/Scripts/Data/DataStore/GenericEntityContainerDataStore.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CAFU.Core.Data.DataStore;
+using CAFU.Core.Utility;
+using CAFU.Generics.Data.Entity;
+
+namespace CAFU.Generics.Data.DataStore
+{
+    public interface IGenericEntityContainerDataStore : IDataStore
+    {
+        void Retain(IGenericEntity genericEntity);
+
+        void Release(IGenericEntity genericEntity);
+
+        TGenericEntity GetEntity<TGenericEntity>() where TGenericEntity : IGenericEntity;
+
+        IEnumerable<TGenericEntity> GetEntities<TGenericEntity>() where TGenericEntity : IGenericEntity;
+    }
+
+    public class GenericEntityContainerDataStore : IGenericEntityContainerDataStore, ISingleton
+    {
+        public class Factory<TDataStore> : DefaultDataStoreFactory<TDataStore> where TDataStore : IDataStore, IGenericEntityContainerDataStore, new()
+        {
+        }
+
+        public class DefaultResolver : IDataStoreResolver<GenericEntityContainerDataStore>, ISingleton
+        {
+            public GenericEntityContainerDataStore Resolve()
+            {
+                return new Factory<GenericEntityContainerDataStore>().Create();
+            }
+        }
+
+        private Dictionary<IGenericEntity, int> ReferenceCountMap { get; } = new Dictionary<IGenericEntity, int>();
+
+        public void Retain(IGenericEntity genericEntity)
+        {
+            if (!ReferenceCountMap.ContainsKey(genericEntity))
+            {
+                ReferenceCountMap[genericEntity] = 0;
+            }
+
+            ReferenceCountMap[genericEntity]++;
+        }
+
+        public void Release(IGenericEntity genericEntity)
+        {
+            if (!ReferenceCountMap.ContainsKey(genericEntity))
+            {
+                return;
+            }
+
+            ReferenceCountMap[genericEntity]--;
+            if (ReferenceCountMap[genericEntity] <= 0)
+            {
+                ReferenceCountMap.Remove(genericEntity);
+            }
+        }
+
+        public TGenericEntity GetEntity<TGenericEntity>() where TGenericEntity : IGenericEntity
+        {
+            if (!ReferenceCountMap.Keys.Any(x => x is TGenericEntity))
+            {
+                throw new InvalidOperationException($"Type of '{typeof(TGenericEntity).FullName}' does not found in GenericDataStore");
+            }
+
+            return (TGenericEntity) ReferenceCountMap.Keys.First(x => x is TGenericEntity);
+        }
+
+        public IEnumerable<TGenericEntity> GetEntities<TGenericEntity>() where TGenericEntity : IGenericEntity
+        {
+            if (!ReferenceCountMap.Keys.Any(x => x is TGenericEntity))
+            {
+                throw new InvalidOperationException($"Type of '{typeof(TGenericEntity).FullName}' does not found in GenericDataStore");
+            }
+
+            return ReferenceCountMap.Keys.OfType<TGenericEntity>();
+        }
+    }
+}
