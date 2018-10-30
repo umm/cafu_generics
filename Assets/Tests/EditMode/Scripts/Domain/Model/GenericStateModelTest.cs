0a9991b9be678b3bd105158df49c1abaafe843b9
Tetsuya Mori <monry84@gmail.com>
2018-10-30 12:04:12 +0900

    :white_check_mark: Write Test

--- Assets/Tests/EditMode/Scripts/Domain/Model/GenericStateModelTest.cs
+++ Assets/Tests/EditMode/Scripts/Domain/Model/GenericStateModelTest.cs
@@ -1,16 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
 
-public class GenericStateModelTest : MonoBehaviour {
+namespace CAFU.Generics.Domain.Model
+{
+    public class GenericStateModelTest
+    {
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        private enum Fixture
+        {
+            Foo,
+            Bar,
+            Baz,
+            Qux,
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [Test]
+        public void EnumNextTest()
+        {
+            var model = new GenericStateModel<Fixture>();
+            Assert.AreEqual(default(Fixture), model.GetCurrent());
+            model.Next();
+            model.Next();
+            Assert.AreEqual(Fixture.Baz, model.GetCurrent());
+            model.Next();
+            Assert.AreEqual(Fixture.Qux, model.GetCurrent());
+            model.Next();
+            model.Next();
+            Assert.AreEqual(Fixture.Qux, model.GetCurrent());
+        }
+
+        [Test]
+        public void EnumPreviousTest()
+        {
+            var model = new GenericStateModel<Fixture>(Fixture.Qux);
+            Assert.AreEqual(Fixture.Qux, model.GetCurrent());
+            model.Previous();
+            Assert.AreEqual(Fixture.Baz, model.GetCurrent());
+            model.Previous();
+            model.Previous();
+            Assert.AreEqual(Fixture.Foo, model.GetCurrent());
+            model.Previous();
+            model.Previous();
+            Assert.AreEqual(Fixture.Foo, model.GetCurrent());
+        }
+
+        [Test]
+        public void PrimitiveTest()
+        {
+            var model1 = new GenericStateModel<int>();
+            model1.Change(2);
+            Assert.Throws<InvalidOperationException>(() => model1.Next());
+            Assert.Throws<InvalidOperationException>(() => model1.Previous());
+            var model2 = new GenericStateModel<bool>();
+            model2.Change(false);
+            Assert.Throws<InvalidOperationException>(() => model2.Next());
+            Assert.Throws<InvalidOperationException>(() => model2.Previous());
+        }
+    }
 }
